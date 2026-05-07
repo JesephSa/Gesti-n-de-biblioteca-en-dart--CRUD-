fed5ed632146b461bb52282d96b79fe8b87d20ff
María Paula <mprymedina@gmail.com>
2026-05-07 09:11:34 -0500

    feat:add menu principal

--- main.dart
+++ main.dart
@@ -1,4 +1,56 @@
-void main(){
+import 'dart:io';
+void main() {
 
+  bool activo = true;
 
+  while (activo) {
+
+    print("\n===== GESTIÓN DE BIBLIOTECA =====");
+    print("1. Agregar libro");
+    print("2. Listar libros");
+    print("3. Buscar libro");
+    print("4. Actualizar libro");
+    print("5. Eliminar libro");
+    print("6. Salir");
+
+    print("\nSeleccione una opción:");
+
+    int opcion = int.parse(stdin.readLineSync()!);
+
+    switch (opcion) {
+
+      case 1:
+        print("Opción agregar libro");
+      break;
+
+      case 2:
+        print("Opción listar libros");
+      break;
+
+      case 3:
+        print("Opción buscar libro");
+      break;
+
+      case 4:
+        print("Opción actualizar libro");
+      break;
+
+      case 5:
+        print("Opción eliminar libro");
+      break;
+
+      case 6:
+        activo = false;
+        print("Programa finalizado");
+      break;
+
+      default:
+        print("Opción inválida");
+    }
+  }
 }
+
+
+
+
+
